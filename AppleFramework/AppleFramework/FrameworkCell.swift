43056040252ef541f163b2bb4fc8a57d4ca88f3c
dgun <petershin89@gmail.com>
2022-09-26 10:11:38 +0900

    Collection View_Framework

---
+++ AppleFramework/AppleFramework/FrameworkCell.swift
@@ -0,0 +1,27 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework
+//
+//  Created by 신 동군 on 2022/09/25.
+//
+
+import UIKit
+
+class FrameworkCell: UICollectionViewCell {
+   
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameLabel.numberOfLines = 1
+        nameLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func configure(_ framework: AppleFramework) {
+        
+        thumbnailImageView.image = UIImage(named: framework.imageName)
+        
+        nameLabel.text = framework.name
+    }
+}
